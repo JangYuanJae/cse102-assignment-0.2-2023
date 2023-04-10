b99ac4b3627534a1c3779a3d15ae0e8662ccdc15
JangYuanJae <serial123@naver.com>
2023-04-11 01:07:21 +0900

    I decorated my first-page.js  more

--- pages/posts/first-page.js
+++ pages/posts/first-page.js
@@ -2,7 +2,12 @@ import Link from 'next/link'
 export default function FirstPage() {
     return (
         <div>
-           <h2>Write your message here!</h2> 
+           <h2>My name is Jang Yuan Jae</h2>
+           <p>I am 26 years old and currently I am 1st grade at Information System.</p>
+           <p>I have little brother, who is 11years younger than me. He is cute</p> 
+           <p>I like soccer. Soccer is fun and exciting.</p>
+           <p>My favorite soccer team is Manchester United in England. </p>
+           
             <Link href="/">Back to home</Link> //GO TO "/"(MAIN PAGE)
         </div>
     )
